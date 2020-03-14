61e3c9d9a91116855ca9f0aee1e6a84676e41bc5
Marek Valko <valko.marek@gmail.com>
2020-03-14 18:56:15 +0100

    Rewrites

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,16 +1,32 @@
 const axios = require('axios')
+const countries = require('./countries.json')
+
 require('dotenv').config()
 
 const API_KEY = process.env.NEWS_API_KEY
 
-const url = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}&pageSize=5`
 
-module.exports = async function() {
+async function getNews(country) {
     try {
+        const url = `http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}&pageSize=5`
         const response = await axios.get(url);
-        return response.data;
+        return {
+            "country": country,
+            "articles": response.data.articles
+
+        }
     } catch (error) {
         console.log(error);
         
     }
+}
+
+
+module.exports = async function() {
+    var newsPromises = countries.map(getNews)
+    return Promise.all(newsPromises).then(newsObjects => {
+        console.log(newsObjects);
+        
+        return [].concat.apply([],newsObjects)
+    })
 }
